47f2b0f026a49f398c8103d056b89df19c6a581d
hmallen <allenhm@gmail.com>
2017-12-10 19:17:12 -0500

    Moved temp csv file creation to isolated function.

--- csv_qr_creator.py
+++ csv_qr_creator.py
@@ -12,64 +12,76 @@ logger = logging.getLogger(__name__)
 address_file = 'btc_addresses.csv'
 temp_file = 'btc_addresses_temp.csv'
 
+debug_mode = True
+
+
+def create_temp_csv():
+    # Copy csv file
+    logger.debug('Copying csv file with BTC addresses to temp file.')
+    shutil.copy(address_file, temp_file)
+    
+    # Read temp csv file
+    address_list = []
+    logger.debug('Reading csv file.')
+    with open(temp_file, newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+        try:
+            for row in csv_reader:
+                #logger.debug(row)
+                
+                num = row[0]
+                pub = row[1].strip('"')
+                priv = row[2].strip('"')
+
+                address_row = [num, pub, priv]
+                #logger.debug(address_row)
+
+                address_list.append(address_row)
+        
+        except Exception as e:
+            logger.exception('Exception occurred while reading csv file.')
+            logger.exception(e)
+            raise
+
+    # Debug print
+    #pprint(address_list)
+
+    # Write modified values to temp csv files
+    with open(temp_file, 'w', newline='') as csv_file:
+        try:
+            for row in address_list:
+                csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(row)
+        
+        except Exception as e:
+            logger.exception('Exception occurred while writing to temp file.')
+            logger.exception(e)
+            raise
+
+
+def create_svg_file():
+    print('Things')
+
+
 if __name__ == '__main__':
     try:
-        # Copy csv file
-        logger.debug('Copying csv file with BTC addresses to temp file.')
-        shutil.copy(address_file, temp_file)
+        create_temp_csv()
+
+        if debug_mode == True:
+            # Debug read of csv file written with modified values
+            with open(temp_file, newline='') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+                try:
+                    for row in csv_reader:
+                        logger.debug(row)
+
+                except Exception as e:
+                    logger.exception('Exception occurred while reading temp file.')
+                    logger.exception(e)
+                    raise
         
-        # Read temp csv file
-        address_list = []
-        logger.debug('Reading csv file.')
-        with open(temp_file, newline='') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
-            try:
-                for row in csv_reader:
-                    #logger.debug(row)
-                    
-                    num = row[0]
-                    pub = row[1].strip('"')
-                    priv = row[2].strip('"')
-
-                    address_row = [num, pub, priv]
-                    #logger.debug(address_row)
-
-                    address_list.append(address_row)
-            
-            except Exception as e:
-                logger.exception('Exception occurred while reading csv file.')
-                logger.exception(e)
-                raise
-
-        # Debug print
-        pprint(address_list)
-
-        # Write modified values to temp csv files
-        with open(temp_file, 'w', newline='') as csv_file:
-            try:
-                for row in address_list:
-                    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                    csv_writer.writerow(row)
-            
-            except Exception as e:
-                logger.exception('Exception occurred while writing to temp file.')
-                logger.exception(e)
-                raise
-
-        # Debug read of csv file written with modified values
-        with open(temp_file, newline='') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
-            try:
-                for row in csv_reader:
-                    logger.debug(row)
-
-            except Exception as e:
-                logger.exception('Exception occurred while reading temp file.')
-                logger.exception(e)
-                raise
-        
-        # For each line, create qr code and save to individual directories
-        print('Things')
+        # For each line, create qr code and save to svg files in individual directories
+        create_svg_file()
 
         # Do something with temp file
         print('Junk')
