0b6fac284af09af43ce8a759739630cf6bb79123
hmallen <allenhm@gmail.com>
2017-12-11 00:29:40 -0500

    Updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSV to QR code creator
+
 Creates QR codes for Bitcoin addresses in csv file generated from "Bulk Wallet" mode on Bitaddress.org.
+
+By default, the program looks for 'btc_addresses.csv' in the root directory. This file should be in the format [row number, public address, private address] to be read properly. If desired, one can use the '-i' or '--input' options to manually define a path to the csv file to be used for QR code creation.
+
+A copy of the input csv file is placed in a newly created directory, stripping unnecessary characters first. The csv file is read line-by-line, creating svg images of QR codes corresponding to the public and private keys. The files are named using the first column in the csv file as a prefix followed by 'pub' or 'priv' to identify the keys. Each set of keys is placed in a unique directory to prevent reuse.
--- csv_qr_creator.py
+++ csv_qr_creator.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import argparse
 import csv
+import datetime
 import logging
+import os
 from pprint import pprint
 from qrcodegen import QrCode, QrSegment
 import shutil
@@ -9,20 +12,35 @@ import sys
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-address_file = 'btc_addresses.csv'
-temp_file = 'btc_addresses_temp.csv'
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', type=str, help='Choose alternate input csv file. [Default = \'btc_addresses.csv\']')
+args = parser.parse_args()
 
-debug_mode = True
+if args.input:
+    address_file = args.input
+    logger.info('Set csv file to input path: \'' + address_file + '\'')
+    
+else:
+    address_file = 'btc_addresses.csv'
+    logger.info('Set csv file to default path: \'' + address_file + '\'')
+
+if not os.path.isfile(address_file):
+    logger.error('File \'' + address_file + '\' does not exist.')
+    if not args.input:
+        logger.error('Confirm default file \'btc_addresses.csv\' exists or define csv path manually using \'--input\'.')
+    sys.exit(1)
+else:
+    logger.info('File \'' + address_file + '\' found.')
 
 
 def create_temp_csv():
     # Copy csv file
-    logger.debug('Copying csv file with BTC addresses to temp file.')
+    logger.debug('Copying csv to temp file.')
     shutil.copy(address_file, temp_file)
     
     # Read temp csv file
     address_list = []
-    logger.debug('Reading csv file.')
+    logger.debug('Reading csv temp file.')
     with open(temp_file, newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
         try:
@@ -59,32 +77,77 @@ def create_temp_csv():
             raise
 
 
-def create_svg_file():
-    print('Things')
+def create_svg():
+    #errcorlvl = QrCode.Ecc.QUARTILE # Error correction level. Need to determine best setting!
+    errcorlvl = QrCode.Ecc.LOW
+    svg_list = []
+
+    with open(temp_file, newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+        try:
+            for row in csv_reader:
+                qr_pub = QrCode.encode_text(row[1], errcorlvl)
+                qr_svg_pub = qr_pub.to_svg_str(4)
+                #logger.debug('PUBLIC')
+                #logger.debug(qr_svg_pub)
+                file_name_pub = output_path + row[0] + '_pub.svg'
+                logger.debug(file_name_pub)
+                with open(file_name_pub, 'w') as svg_file:
+                    svg_file.write(qr_svg_pub)
+                
+                qr_priv = QrCode.encode_text(row[2], errcorlvl)
+                qr_svg_priv = qr_priv.to_svg_str(4)
+                #logger.debug('PRIVATE')
+                #logger.debug(qr_svg_priv)
+                file_name_priv = output_path + row[0] + '_priv.svg'
+                logger.debug(file_name_priv)
+                with open(file_name_priv, 'w') as svg_file:
+                    svg_file.write(qr_svg_priv)
+
+        except Exception as e:
+            logger.exception('Exception while creating svg files.')
+            logger.exception(e)
+            raise
 
 
 if __name__ == '__main__':
+    if not os.path.exists('output'):
+        logger.info('Output directory not found. Creating.')
+        try:
+            os.makedirs('output')
+        except Exception as e:
+            logger.exception('Failed to create output directory. Exiting.')
+            logger.exception(e)
+            sys.exit(1)
+    else:
+        logger.info('Found output directory.')
+
+    dt_current = datetime.datetime.now().strftime('%m%d%Y-%H%M%S')
+
+    temp_file = 'output/' + dt_current + '/btc_addresses_temp.csv'
+
     try:
+        output_path = 'output/' + dt_current + '/'
+        os.makedirs(output_path)
+        
         create_temp_csv()
 
-        if debug_mode == True:
-            # Debug read of csv file written with modified values
-            with open(temp_file, newline='') as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
-                try:
-                    for row in csv_reader:
-                        logger.debug(row)
+        """
+        # Debug read of csv file written with modified values
+        with open(temp_file, newline='') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+            try:
+                for row in csv_reader:
+                    logger.debug(row)
 
-                except Exception as e:
-                    logger.exception('Exception occurred while reading temp file.')
-                    logger.exception(e)
-                    raise
-        
-        # For each line, create qr code and save to svg files in individual directories
-        create_svg_file()
+            except Exception as e:
+                logger.exception('Exception occurred while reading temp file.')
+                logger.exception(e)
+                raise
+        """
 
-        # Do something with temp file
-        print('Junk')
+        # For each line, create qr code and handle svg files
+        create_svg()
 
     except Exception as e:
         logger.exception(e)
